fcba2a59f1e08b19ed3844cfbf0cf2ce6edc077d
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-07-04 22:35:22 +0600

    Added screenshot

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<title>Laravel blog @yield('title')</title>
+<title>Laravel Book Review @yield('title')</title>
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
